5076108f38132e0253e630c7c5945c622e814960
anshurw <anshurw@gmail.com>
2023-06-16 22:20:01 +0530

    update

--- VrHorrorGameTemplate/Assets/FlashLightGrab.cs
+++ VrHorrorGameTemplate/Assets/FlashLightGrab.cs
@@ -4,30 +4,41 @@ using UnityEngine;
 
 public class FlashLightGrab : MonoBehaviour
 {
-    public OVRInput.Button OnButton,GrabButton;
+    public OVRInput.Button OnButton, GrabButton;
     public FlashlightToggle flashLightToggle;
     public bool bistriggerPressed;
+
     OVRGrabbable grabbable;
     AudioSource FlashLightAudio;
+
     // Start is called before the first frame update
     void Start()
     {
         grabbable = GetComponent<OVRGrabbable>();
         FlashLightAudio = GetComponent<AudioSource>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        bistriggerPressed = OVRInput.Get(GrabButton,grabbable.grabbedBy.GetController());
-        if (grabbable.isGrabbed && OVRInput.GetDown(OnButton,grabbable.grabbedBy.GetController()))
+        if (grabbable.isGrabbed && OVRInput.GetDown(OnButton, grabbable.grabbedBy.GetController()))
         {
             flashLightToggle.ToggleOn();
+            //grabbable.grabbedBy.ForceRelease(grabbable);
         }
-        if(grabbable.isGrabbed&&OVRInput.GetDown(GrabButton,grabbable.grabbedBy.GetController()))
-        {
-            grabbable.grabbedBy.ForceRelease(grabbable);
-        }
+       if(grabbable.isGrabbed && OVRInput.Get(GrabButton, grabbable.grabbedBy.GetController()))
+            {
+                GrabObject();
+            }
+        
+
+    }
+    public void GrabObject()
+    {
+       
+         grabbable.grabbedBy.ForceRelease(grabbable);
+       
+          
+        
     }
 }
